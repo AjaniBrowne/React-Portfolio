698ffa96b8f0be9a9fdb0758a7d355ed66c2da0d
AjaniBrowne <ajani.browne@yahoo.com>
2022-09-07 20:29:09 -0400

    updates
    added projects to portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import travelersguide from '../images/travelers guide.png';
+import caninecollective from '../images/canine-collective.png';
+import soleplane from '../images/soleplane.png'
 
 
 export default function Portfolio() {
     return (
-      <div className = 'bg-danger'>
+      <div >
         <h1>Coding Projects</h1>
         <figure>
-           <a href="https://ajanibrowne.github.io/Traveler-s-Guide/"> <img src="./group project.jpeg" width="200px" alt="travel agents"/></a>
+           <a href="https://ajanibrowne.github.io/Traveler-s-Guide/"> <img src={travelersguide} width="200px" alt="travel agents"/></a>
         </figure>
         <figure>
-            <a href="https://hidden-eyrie-39114.herokuapp.com/"> <img src="./canine-collective.png" width="200px" alt="canine collective"/></a>
+            <a href="https://hidden-eyrie-39114.herokuapp.com/"> <img src={caninecollective} width="200px" alt="canine collective"/></a>
+        </figure>
+        <figure>
+           <a href="https://sole-plane-official.herokuapp.com/"> <img src={soleplane} width="200px" alt="soleplane"/></a>
         </figure>
       </div>
     );
